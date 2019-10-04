b7f9bba2545be8b834cfa1454c78f7d7a38130f4
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-04 22:32:00 +0300

    Update Main.java

--- src/hw_1/task_1/Main.java
+++ src/hw_1/task_1/Main.java
@@ -1,6 +1,7 @@
 package HW_1.Task1;
 
 public class Main {
+    // String...args -> лучше все же вот так 'String[] args', хотя технически разницы нету
     public static void main(String...args){
         System.out.println("Hola, " + args[0] + "!");
     }
