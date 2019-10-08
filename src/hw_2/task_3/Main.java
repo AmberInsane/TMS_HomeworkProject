447c67e85a8eee1f68d1ec7faf783b2e0ba2f645
AmberInsane <marrycitty@mail.ru>
2019-10-08 21:13:41 +0300

    l2.t3

---
+++ src/hw_2/task_3/Main.java
@@ -0,0 +1,13 @@
+package hw_2.task_3;
+/*
+Вычислить: 1+2+4+8+...+256
+ */
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 256; i *= 2) {
+            sum += i;
+        }
+        System.out.println("1+2+4+8+...+256=" + sum);
+    }
+}
