0d6ec96ddd7c9048d14e74ecb41e900d38a9103e
AmberInsane <marrycitty@mail.ru>
2019-10-08 21:44:21 +0300

    l2.t5

---
+++ src/hw_2/task_6/Main.java
@@ -0,0 +1,13 @@
+package hw_2.task_6;
+/*
+Напишите программу вывода всех четных чисел от 2 до 100 включительно
+ */
+public class Main {
+    public static void main(String[] args) {
+        int i = 2;
+        while (i <= 100) {
+            System.out.print("\t"+i);
+            i +=2;
+        }
+    }
+}
