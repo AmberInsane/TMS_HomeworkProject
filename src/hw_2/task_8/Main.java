04f1bb7b2fa66876dbae765ca9633db2b9c6102a
AmberInsane <marrycitty@mail.ru>
2019-10-08 21:57:38 +0300

    l2.t6-8

---
+++ src/hw_2/task_8/Main.java
@@ -0,0 +1,26 @@
+package hw_2.task_8;
+/*
+Даны числа от 0 до 100.
+- Посчитать сумму четных и вывести на экран
+- Посчитать сумму нечетных и вывести на экран
+- Найти общую сумму всех чисел
+ */
+public class Main {
+    public static void main(String[] args) {
+        int oddSum, evenSum;
+        evenSum = 2; //the first not zero even num
+        oddSum = 1; //the first oddSum num
+
+        int iterations = (100 - 2)/2;
+        while (iterations > 0){
+            //pause
+            iterations --;
+        }
+
+
+
+
+
+
+    }
+}
