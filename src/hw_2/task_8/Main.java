52aee7675610c50d158ff632bcc37ee0ae128df5
AmberInsane <marrycitty@mail.ru>
2019-10-09 23:38:52 +0300

    hw2.t8

--- src/hw_2/task_8/Main.java
+++ src/hw_2/task_8/Main.java
@@ -7,20 +7,39 @@ package hw_2.task_8;
  */
 public class Main {
     public static void main(String[] args) {
-        int oddSum, evenSum;
-        evenSum = 2; //the first not zero even num
-        oddSum = 1; //the first oddSum num
+        int oddNum, evenNum, maxOddNum, maxEvenNum, sumOdd, sumEven;
+        evenNum = 2; //the first not zero even num
+        oddNum = 1; //the first oddSum num
+        maxEvenNum = 100;
+        maxOddNum = maxEvenNum - 1;
 
-        int iterations = (100 - 2)/2;
-        while (iterations > 0){
-            //pause
-            iterations --;
+        sumOdd = 0;
+        sumEven = 0;
+
+        while (oddNum <= maxEvenNum){
+            sumEven += evenNum;
+            sumOdd += oddNum;
+
+            evenNum +=2;
+            oddNum +=2;
         }
+        System.out.println("Sum of even numbers from 0 to 100 is " + sumEven);
+        System.out.println("Sum of odd numbers from 0 to 100 is " + sumOdd);
+        System.out.println("Sum of all numbers from 0 to 100 is " + (sumOdd + sumEven));
 
+        //or if we don't need to use cycles, we can use the formula of sum of arithmetic progression
+        evenNum = 2; //the first not zero even num
+        oddNum = 1; //the first oddSum num
 
+        int numberOfEven =  (maxEvenNum - evenNum)/2 + 1;
+        int numberOfOdd =  (maxOddNum - oddNum)/2 + 1;
 
+        sumEven = numberOfEven * (maxEvenNum + evenNum)/2;
+        sumOdd = numberOfOdd * (maxOddNum + oddNum)/2;
 
-
-
+        System.out.println("\nSum of arithmetic progression:");
+        System.out.println("Sum of even numbers from 0 to 100 is " + sumEven);
+        System.out.println("Sum of odd numbers from 0 to 100 is " + sumOdd);
+        System.out.println("Sum of all numbers from 0 to 100 is " + (sumOdd + sumEven));
     }
 }
