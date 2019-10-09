d9f0e427b71b4788ca318f818f2d829c982c2bab
AmberInsane <marrycitty@mail.ru>
2019-10-10 00:09:26 +0300

    hw2.t8

--- src/hw_2/task_4/Main.java
+++ src/hw_2/task_4/Main.java
@@ -4,20 +4,34 @@ package hw_2.task_4;
 public class Main {
     public static void main(String[] args) {
         int fistNum, secondNum, buffer, multResult;
+        int isPos = 1;
         multResult = 0;
-        fistNum = 7;
-        secondNum = 9;
-        if (fistNum > secondNum){
-            //меняем местами, чтобы итераций было меньше
-            buffer = fistNum;
-            fistNum = secondNum;
-            secondNum = buffer;
-        }
-        for (int i = 0; i < fistNum; i++) {
-            multResult += secondNum;
-        }
-        System.out.println(fistNum + " * " + secondNum + " = " + multResult);
+        try {
+            fistNum = Integer.parseInt(args[0]);
+            secondNum = Integer.parseInt(args[1]);
 
+            System.out.print(fistNum + " * " + secondNum + " = ");
+
+            if ((fistNum > 0 && secondNum < 0) || (fistNum < 0 && secondNum > 0)) {
+                isPos = -1;
+                fistNum = Math.abs(fistNum);
+                secondNum = Math.abs(secondNum);
+            }
+
+            if (fistNum > secondNum) {
+                //swap to get less number of iterations
+                buffer = fistNum;
+                fistNum = secondNum;
+                secondNum = buffer;
+            }
+            for (int i = 0; i < fistNum; i++) {
+                multResult += secondNum;
+            }
+
+            multResult *= isPos;
+            System.out.println(multResult);
+        } catch (Exception e) {
+            System.out.println("You should put in only integers");
+        }
     }
-
 }
