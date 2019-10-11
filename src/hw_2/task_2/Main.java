bd924aba200ca09880ede1f6b5a05ff5e6ba21e8
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-11 17:46:54 +0300

    Update Main.java

--- src/hw_2/task_2/Main.java
+++ src/hw_2/task_2/Main.java
@@ -2,6 +2,8 @@ package hw_2.task_2;
 /*Одноклеточная амеба каждые 3 часа делится на 2 клетки. Определить,
 сколько амеб будет через 3, 6, 9, 12,..., 24 часа.
  */
+
+// +
 public class Main {
     public static void main(String[] args) {
         int tempCount = 1; //текущее количество амеб
