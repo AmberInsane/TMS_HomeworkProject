e76d1d0ccb054a324fbf707e9d8b3a0f3f6a02b9
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-13 12:51:02 +0300

    Update Butterfly.java

--- src/hw_3/task_8/Butterfly.java
+++ src/hw_3/task_8/Butterfly.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 /*Нарисовать на консоли бабочку. Пример вывода в butterfly.png
   Как пользователь, я бы хотел иметь возможность задать размер бабочки (высоту и ширину)
 */
+
+// бабочка супер, молодец
 public class Butterfly {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
@@ -18,22 +20,22 @@ public class Butterfly {
             width = scanner.nextInt();
 
         } catch (Exception e) {
-            System.out.println("Ok, I'll show you butterfly anyway");
+            System.out.println("Ok, I'll show you butterfly anyway"); // top-1 :)
             height = 17;
             width = 17;
         }
-        int k;
+        int k; // k - так себе название
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (i >= height / 2)
+                if (i >= height / 2) // {}
                     k = height - i - 1;
-                else
+                else // {}
                     k = i;
 
                 if ((j <= k) || (j >= width - k - 1)) {
-                    if (j >= width / 2)
+                    if (j >= width / 2) // {}
                         k = width - j;
-                    else
+                    else // {}
                         k = j + 1;
                     System.out.print(k);
                 } else
