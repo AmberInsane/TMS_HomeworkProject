0b539c90d4846a2f571b49b4b0ce55cf5feefbb0
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-13 12:30:31 +0300

    Update Main.java

--- src/hw_3/task_2/Main.java
+++ src/hw_3/task_2/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 //Есть массив чисел. Найти среднее арифметическое число элементов массива
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int [10];
+        int[] array = new int [10]; // int [10] - пробел не нужен после int
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 50);
         }
