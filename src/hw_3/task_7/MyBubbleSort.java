4e5eea7823d1133a33ccc4c9c2477a556e434250
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-13 12:43:24 +0300

    Update MyBubbleSort.java

--- src/hw_3/task_7/MyBubbleSort.java
+++ src/hw_3/task_7/MyBubbleSort.java
@@ -13,18 +13,20 @@ public class MyBubbleSort {
         System.out.println("Before bubble sort:");
         System.out.println(Arrays.toString(array));
 
+        // мне кажется или код не отформатирован?
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i; j < array.length; j++) {
                 if (array[i] > array[j])
                     swap(array, i, j);
             }
-
+// пустая строка не нужна
         }
 
         System.out.println("After bubble sort:");
         System.out.println(Arrays.toString(array));
     }
 
+    // i,j - так себе имена переменных
     private static void swap(int[] arr, int i, int j) {
         int temp;
         temp = arr[i];
