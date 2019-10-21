1c9a782b5f78d8f8d36273a27cf29010b4c81112
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-21 21:02:16 +0300

    Update Computer.java

--- src/hw_5/task_1/Computer.java
+++ src/hw_5/task_1/Computer.java
@@ -1,5 +1,6 @@
 package hw_5.task_1;
 
+// дефолтные методы старайся не использовать, на занятии расскажу чуть подробнее 
 public interface Computer {
     default void turnOn() {
         System.out.println("I'm alive!");
