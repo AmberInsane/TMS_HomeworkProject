439d2a3cce8da1114edd487ab4dd003caaea0bf1
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-21 21:15:03 +0300

    Update WarehouseMain.java

--- src/hw_5/task_2/WarehouseMain.java
+++ src/hw_5/task_2/WarehouseMain.java
@@ -9,6 +9,8 @@ import java.util.Arrays;
 Если дошли до последнего элемента при вызове next(), должно вернуть первый элемент
 также должен быть метод getArray() - который возвращает массив и переопределен метод toString()
  */
+
+// форматируй код
 public class WarehouseMain {
     public static void main(String[] args) throws CloneNotSupportedException {
         Warehouse warehouse = new Warehouse(new Box[]{
@@ -18,6 +20,7 @@ public class WarehouseMain {
                 new Box("B-39", 19.5, 700),
                 new Box("C-11", 13.3, 300)
         });
+        // warehouse1 - так себе название
         Warehouse warehouse1 = new Warehouse(new Box[]{});
         System.out.println(warehouse1);
         System.out.println(Arrays.toString(warehouse1.getBoxes()));
