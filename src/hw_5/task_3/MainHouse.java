2364eec248729bb30bba8963b22da59fc913ac8c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-21 21:26:48 +0300

    Update MainHouse.java

--- src/hw_5/task_3/MainHouse.java
+++ src/hw_5/task_3/MainHouse.java
@@ -2,6 +2,7 @@ package hw_5.task_3;
 
 import hw_5.task_3.houses.*;
 
+// +
 public class MainHouse {
     public static void main(String[] args) {
         System.out.println("=====Country house=====");
