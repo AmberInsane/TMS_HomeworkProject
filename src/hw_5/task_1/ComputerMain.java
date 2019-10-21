39033cff59d0c99c34ed3b5b39f2e452ff757e25
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-21 21:03:14 +0300

    Update ComputerMain.java

--- src/hw_5/task_1/ComputerMain.java
+++ src/hw_5/task_1/ComputerMain.java
@@ -2,6 +2,7 @@ package hw_5.task_1;
 /*Есть интерфейс Computer,у которого есть методы включить, выключить и резет. Создайте класс, который бы имплементировал
 этот интерфейс
  */
+//+
 public class ComputerMain {
     public static void main(String[] args) {
         Computer laptop = new Laptop();
