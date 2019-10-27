56f0e9da68c49fdbd6a3544af7186a890918849f
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-27 14:13:11 +0300

    Update Transmission.java

--- src/hw_6/task_2/Transmission.java
+++ src/hw_6/task_2/Transmission.java
@@ -5,13 +5,15 @@ package hw_6.task_2;
 Нельзя понизить передачу ниже 0 и повысить больше 7.
  */
 public class Transmission {
+    // + final
     private static int MIN_GEAR = 0;
+    // + final
     private static int MAX_GEAR = 7;
 
     private int gear;
 
     public Transmission() {
-        this.gear = MIN_GEAR;
+        this.gear = MIN_GEAR; // эта строка не нужна, у тебя gear по умолчанию 0, как и MIN_GEAR
     }
 
     public void upTransmission() {
