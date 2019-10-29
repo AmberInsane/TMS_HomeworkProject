1533c1202248699348a8d504952a4d2f9bdf059b
AmberInsane <marrycitty@mail.com>
2019-10-29 16:12:26 +0300

    hw_7.done

--- src/hw_7/task_8/Car.java
+++ src/hw_7/task_8/Car.java
@@ -1,18 +1,12 @@
 package hw_7.task_8;
 
-import hw_6.task_8.Engine;
-
 public class Car {
     private String name;
-    private hw_6.task_8.Engine engine;
+    private Engine engine;
 
-    public Car(String name, hw_6.task_8.Engine engine) {
+    public Car(String name, Engine engine) {
         this.name = name;
         this.engine = engine;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public Engine getEngine() {
--- src/hw_7/task_8/EngineRenameMain.java
+++ src/hw_7/task_8/EngineRenameMain.java
@@ -1,28 +1,25 @@
 package hw_7.task_8;
 
-import hw_6.task_8.Car;
-import hw_6.task_8.Engine;
-
 import java.util.Arrays;
 
 public class EngineRenameMain {
     public static void main(String[] args) {
-        hw_6.task_8.Car[] cars = new hw_6.task_8.Car[5];
-        cars[0] = new hw_6.task_8.Car("1111", new hw_6.task_8.Engine("1.4T", 1.4));
-        cars[1] = new hw_6.task_8.Car("2222", new hw_6.task_8.Engine("1.5T", 1.5));
-        cars[2] = new hw_6.task_8.Car("3423", new hw_6.task_8.Engine("1.8R", 1.8));
-        cars[3] = new hw_6.task_8.Car("2342", new hw_6.task_8.Engine("1.9R", 1.9));
+        Car[] cars = new Car[5];
+        cars[0] = new Car("1111", new  Engine("1.4T", 1.4));
+        cars[1] = new Car("2222", new Engine("1.5T", 1.5));
+        cars[2] = new Car("3423", new Engine("1.8R", 1.8));
+        cars[3] = new Car("2342", new Engine("1.9R", 1.9));
         cars[4] = new Car("1231", new Engine("1.0", 1.4));
 
         System.out.println("Before changing:");
         System.out.println(Arrays.toString(cars));
 
-        for (Car car : cars) {
-            if (car.getEngine().getVolume() == 1.4) {
-                car.getEngine().setName("2.0Т");
+        for (int i = 0; i < cars.length; i++) {
+            if (cars[i].getEngine().getVolume() == 1.4) {
+                cars[i].getEngine().setName("2.0Т");
             }
-            if (car.getEngine().getVolume() == 1.8) {
-                car.getEngine().setName("1.8Т");
+            if (cars[i].getEngine().getVolume() == 1.8) {
+                cars[i].getEngine().setName("1.8Т");
             }
         }
 
