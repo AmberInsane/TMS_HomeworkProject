7dea38f1afcb39e62762e831b184138840ad8a83
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-05 20:29:23 +0300

    Update WriteSecretMessage.java

--- src/hw_9/task_6/WriteSecretMessage.java
+++ src/hw_9/task_6/WriteSecretMessage.java
@@ -5,12 +5,13 @@ import java.io.*;
 public class WriteSecretMessage {
     private static final String FILE_PATH = "src/hw_9/task_6/imj.jpeg";
 
+    // final.            secret -> SECRET
     private static String secret = "42 is the Answer to the Ultimate Question of Life, the Universe, and Everything";
 
     public static void main(String[] args) {
         try {
             File file = new File(FILE_PATH);
-            if (!file.exists() && !file.createNewFile()) {
+            if (!file.exists() && !file.createNewFile()) { // Task_1
                 throw new IOException();
             }
             writeSecretToFile(secret, file);
@@ -19,7 +20,7 @@ public class WriteSecretMessage {
         }
     }
 
-    private static void writeSecretToFile(String secret, File file){
+    private static void writeSecretToFile(String secret, File file){ // вроде не отформатирован код?
         try (FileOutputStream fos = new FileOutputStream(file, true)){
             fos.write(("\n" + secret).getBytes());
             fos.flush();
