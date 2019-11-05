67eed593e0d4407c34e9bda419fbff33ffcf9d78
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-05 20:39:51 +0300

    Update ReadSecretMessage.java

--- src/hw_9/task_6/ReadSecretMessage.java
+++ src/hw_9/task_6/ReadSecretMessage.java
@@ -8,13 +8,14 @@ public class ReadSecretMessage {
     public static void main(String[] args) {
         File file = new File(FILE_PATH);
         String secret = readSecretFromFile(file);
-        if (!secret.equals("")) {
+        if (!secret.equals("")) { // если возможно старайся не использовать !, можно просто поменять сообщения местами
             System.out.print("Secret message: " + secret);
         } else {
             System.out.print("Secret not found");
         }
     }
 
+    // можно и так, но если в файл дописать еще что-нибудь, то секретное сообщение будет прочитано неверно
     private static String readSecretFromFile(File file) {
         String secret = "";
         try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
