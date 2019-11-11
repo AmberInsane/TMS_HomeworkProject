5bc10ca900c4a18457978ee458a5136abfa6f511
Mary Stankevich <marrycitty@mail.ru>
2019-11-11 07:48:34 +0300

    Update GarageMain.java

--- src/hw_10/task_3/GarageMain.java
+++ src/hw_10/task_3/GarageMain.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 
 public class GarageMain {
     public static void main(String[] args) {
-        Car[] cars = new Car[]{new Lada(13242344, (short) 2012, Color.BLACK),
-                new Lada(324234324, (short) 2018, Color.ORANGE),
-                new BMW(586869865, (short) 2000, Color.BLUE)};
+        Car[] cars = new Car[]{new Lada(132423567844L, (short) 2012, Color.BLACK),
+                new Lada(32427734324L, (short) 2018, Color.ORANGE),
+                new BMW(586899069865L, (short) 2000, Color.BLUE)};
 
         Garage<Car> allCarsGarage = new Garage<>("Ololo st., 29");
         Garage<BMW> onlyBMWGarage = new Garage<>("Ololo st., 30", new ArrayList<>());
