63bda0458f1efe9c0b3bf76fdca4ba9e36ecb262
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-12 21:38:24 +0300

    Update MainBox.java

--- src/hw_10/task_1/MainBox.java
+++ src/hw_10/task_1/MainBox.java
@@ -2,6 +2,7 @@ package hw_10.task_1;
 
 //Создать обобщенный (generic) класс, который в конструкторе принимает аргумент неизвестного типа. Создайте  в классе метод,
 //который бы выводил что это за тип на экран (получить имя класса для переменной getClass().getName())
+// +
 public class MainBox {
     public static void main(String[] args) {
         Box<Sock> myBox = new Box<>(new Sock());
