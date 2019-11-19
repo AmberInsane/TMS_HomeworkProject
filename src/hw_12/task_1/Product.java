d3ac3fcf3e88ff8227d776bb388191606187ec86
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-19 23:07:45 +0300

    Update Product.java

--- src/hw_12/task_1/Product.java
+++ src/hw_12/task_1/Product.java
@@ -3,6 +3,7 @@ package hw_12.task_1;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+// +
 public class Product {
     private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
