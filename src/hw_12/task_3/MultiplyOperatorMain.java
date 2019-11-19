ff632a550f7ce9e27a1e8949afe766a2ca8510a4
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-19 23:09:14 +0300

    Update MultiplyOperatorMain.java

--- src/hw_12/task_3/MultiplyOperatorMain.java
+++ src/hw_12/task_3/MultiplyOperatorMain.java
@@ -2,6 +2,7 @@ package hw_12.task_3;
 
 import java.util.function.BinaryOperator;
 
+// +
 public class MultiplyOperatorMain {
     public static void main(String[] args) {
         BinaryOperator<Integer> multiplyOperator = (x, y) -> x * y;
