7a52b716c7c49e73145421a68be73412cb352aa5
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-22 20:21:28 +0300

    Update DistinctMain.java

--- src/hw_13/task_6/DistinctMain.java
+++ src/hw_13/task_6/DistinctMain.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// +
 public class DistinctMain {
     public static void main(String[] args) {
         List<Phone> phones = new ArrayList<>();
