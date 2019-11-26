1e7372315cebff8ad3b8a8f82308c0898b8dd95b
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-26 23:08:43 +0300

    Update PalindromesMain.java

--- src/hw_14/task_4/PalindromesMain.java
+++ src/hw_14/task_4/PalindromesMain.java
@@ -1,5 +1,6 @@
 package hw_14.task_4;
 
+// +
 public class PalindromesMain {
     public static void main(String[] args) {
         System.out.println(StringUtils.getPalindromes("камень, бакаб и пороороп!"));
