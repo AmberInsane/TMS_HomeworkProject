6e5b0266b691a6b9eb20244c31cc829d146e604f
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-26 23:09:42 +0300

    Update CatFileRider.java

--- src/hw_14/task_5/CatFileRider.java
+++ src/hw_14/task_5/CatFileRider.java
@@ -3,6 +3,6 @@ package hw_14.task_5;
 import java.io.File;
 import java.util.List;
 
-public interface CatFileRider {
+public interface CatFileRider { // Reader
     List<Cat> readCatFromFile(File file);
 }
