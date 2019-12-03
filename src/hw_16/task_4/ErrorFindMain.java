e9cf81a84d7aa20d345d7254af990343c904089c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-12-03 20:05:15 +0300

    Update ErrorFindMain.java

--- src/hw_16/task_4/ErrorFindMain.java
+++ src/hw_16/task_4/ErrorFindMain.java
@@ -3,6 +3,7 @@ package hw_16.task_4;
 import java.util.ArrayList;
 import java.util.List;
 
+// +
 public class ErrorFindMain {
     public static void main(String[] args) {
         List<String> words = new ArrayList<>();
