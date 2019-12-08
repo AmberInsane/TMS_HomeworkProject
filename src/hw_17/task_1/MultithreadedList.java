54dd4660af366afa451f27f203796069b7a11c21
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-12-08 11:56:10 +0300

    Update MultithreadedList.java

--- src/hw_17/task_1/MultithreadedList.java
+++ src/hw_17/task_1/MultithreadedList.java
@@ -29,7 +29,7 @@ public class MultithreadedList<T> {
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
-            locker.unlock();
+            locker.unlock(); // не получала IllegalMonitorStateException?
         }
     }
 }
