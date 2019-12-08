c5ff51a49c25334b0b9ce3d4d2d3790cf4389853
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-12-08 11:21:51 +0300

    Update AverageStreamAPI.java

--- src/hw_17/task_2/AverageStreamAPI.java
+++ src/hw_17/task_2/AverageStreamAPI.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
 
+// +
 public class AverageStreamAPI {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
