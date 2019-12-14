119c7da96505067dd830d274ce7fe43fb19592d2
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-12-14 15:34:57 +0300

    Update Tunnel.java

--- src/main/java/hw_17/task_6/Tunnel.java
+++ src/main/java/hw_17/task_6/Tunnel.java
@@ -2,6 +2,7 @@ package hw_17.task_6;
 
 import java.util.concurrent.Semaphore;
 
+// не вижу смысла в этом классе, нового он ничего не добавил
 public class Tunnel extends Semaphore {
 
     public Tunnel(int permitsNumber) {
